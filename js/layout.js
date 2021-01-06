561cebf591a61dec974d034d73b245bbe9b4b83e
kyj933 <dbwlsdl904@hanmail.net>
2021-01-06 13:14:19 +0900

    최종

--- js/layout.js
+++ js/layout.js
@@ -123,6 +123,105 @@ var lypopClose = function(e, _this) {
 
 
 
+$(function() { 
+
+var pshparell = { }
+pshparell.winHT = $(window).height();
+pshparell.distance = pshparell.winHT;
+pshparell.count = 0;
+pshparell.mnoving = false;	
+pshparell.length = $(document).find(".box").length;
+	
+	$(".box").each(function (index, element) { 
+		
+		$(element).on("mousewheel DOMMouseScroll", function (e) {
+			//console.log($(this).html());
+			
+			//console.log("e ====", e);
+			//console.log("originalEvent ====", e.originalEvent);
+			
+			e.preventDefault();
+
+			var El = e.originalEvent;			
+
+			var delta = 0;
+
+			if (El.wheelDelta) {
+				delta = event.wheelDelta / 120;
+				//if (window.opera) delta = -delta;
+			} else if (El.detail) delta = -El.detail / 3;		
+
+			//console.log("dd?????? ==", El.detail);
+			//console.log("event.wheelDelta ==", event.wheelDelta);
+
+			
+			//console.log("next", $(window).scrollTop());
+			//console.log("moveTop", moveTop);
+			
+			
+			if(pshparell.mnoving == false) { 				
+				pshparell.mnoving = true;
+				
+                
+             
+				// 마우스휠이 위에서 아래로 돌다가 마지막으로 도달했을 때			
+				if (delta  < 0 ) {
+					if($(window).scrollTop() == pshparell.distance * (pshparell.length-1)) {
+						moveTop = $(this).offset().top;	
+                     
+					} else { 
+						moveTop = $(this).next().offset().top;					  
+					}
+				// 마우스휠을 아래에서 위로
+				} else {
+					if($(window).scrollTop() < pshparell.distance) { 
+					   moveTop = $(this).offset().top;
+					} else { 
+						moveTop = $(this).prev().offset().top;
+					}	
+				}
+
+				
+				
+				$("html,body").stop().animate({
+					scrollTop: moveTop + 'px'
+				}, {
+					duration: 800, complete: function () {
+						pshparell.mnoving = false;	
+					}
+				});	
+			}
+			
+		});
+	});
+	
+	
+	
+	//var itemleng = $('.box').length;
+	var scfunc = function(event) { 
+		event.preventDefault();
+		var _this = $(this);
+		var _thispt = $(this).parent();
+		var _target = $(event).target;
+		var _index = _thispt.index(_target);
+            
+		pshparell.count = _index;
+        /* 
+            0 * 937
+            1 * 937
+            2 * 937
+            
+        */
+		var scrollHT = 	pshparell.distance * pshparell.count;
+		console.log("scrollHT", scrollHT);
+		
+		$("html,body").stop().animate({scrollTop : scrollHT + 'px'}, 500);	
+	};
+	
+
+
+	
+})	
 
 
 
